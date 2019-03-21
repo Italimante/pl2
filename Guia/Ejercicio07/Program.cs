676dbd16d2775f6c606ef90c4ba1ddb1fcba6727
Andy <italimante@gmail.com>
2019-03-21 18:53:17 -0300

    Ejercicio 07

--- Guia/Ejercicio07/Program.cs
+++ Guia/Ejercicio07/Program.cs
@@ -10,6 +10,27 @@ namespace Ejercicio07
     {
         static void Main(string[] args)
         {
+            string dia;
+            string mes;
+            string anio;
+            string aux;
+            DateTime fechaNacimiento;
+
+            Console.WriteLine("[!] - Ingresar día de nacimiento");
+            dia = Console.ReadLine();
+            Console.WriteLine("[!] - Ingresar número de mes de nacimiento");
+            mes = Console.ReadLine();
+            Console.WriteLine("[!] - Ingresar año de nacimiento");
+            anio = Console.ReadLine();
+
+            aux = dia + "/" + mes + "/" + anio;
+
+            fechaNacimiento = DateTime.Parse(aux);
+
+            Console.WriteLine("Resultado: {0}", DateTime.Now - fechaNacimiento);
+
+            Console.ReadKey();
+
         }
     }
 }
