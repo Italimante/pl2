f0c6ef3af38268b218c0816f8f15a580fb74f40d
italimante <italimante@gmail.com>
2019-04-02 21:54:00 -0300

    17 a medias

--- Guia/Ejercicio17/Boligrafo.cs
+++ Guia/Ejercicio17/Boligrafo.cs
@@ -8,6 +8,56 @@ namespace Ejercicio17
 {
     class Boligrafo
     {
+        #region ATRIBUTOS
+        public const short cantidadTintaMaxima = 100;
+        private ConsoleColor _color;
+        private short _tinta;
+        #endregion
+
+        #region CONSTRUCTORES
+
+        public Boligrafo(short tinta, ConsoleColor color)
+        {
+            this._color = color;
+            this._tinta = tinta;
+        }
+
+        #endregion
+
+        #region METODOS
+        public ConsoleColor GetColor()
+        {
+            return this._color;
+        }
+
+        public short GetTinta()
+        {
+            return this._tinta;
+        }
+
+        private void SetTinta(short tinta)
+        {
+            if( this._tinta + tinta >= 0 && this._tinta + tinta <= cantidadTintaMaxima)
+            {
+                this._tinta += tinta;
+            }
+        }
+
+        public void Recargar()
+        {
+            this._tinta = cantidadTintaMaxima;
+        }
+
+        public bool Pintar(int gasto, out string dibujo)
+        {
+            this._tinta -= gasto;
+            dibujo = "*";
+            return true;
+        }
+        
+        
+        #endregion
+
 
     }
 }
