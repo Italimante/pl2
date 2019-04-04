bbe6200cd49b676e2c1878ffa4ac7a63407c4171
Andy <italimante@gmail.com>
2019-04-04 20:13:20 -0300

    19 a cuartos y ptt's

---
+++ Guia/Ejercicio20/Dolar.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio20
+{
+    public class Dolar
+    {
+        #region ATRIBUTOS
+
+        private double _cantidad;
+        private float _cotizRespectoDolar;
+
+        #endregion
+
+        #region CONSTRUCTORES
+
+        private Dolar()
+        {
+
+        }
+
+        public Dolar(double cantidad)
+        {
+
+        }
+
+        public Dolar(double cantidad, float cotizacion)
+        {
+
+        }
+
+        #endregion
+
+        #region GETs
+
+        public double GetCantidad()
+        {
+            return 1;
+        }
+
+        public float GetCotizacion()
+        {
+            return 0;
+        }
+
+        #endregion
+
+        #region SOBRECARGAS ENTRE MONEDAS
+
+        public static explicit operator Euro(Dolar d){}
+        public static explicit operator Pesos(Dolar d){}
+        public static implicit operator Dolar(double d){}
+
+        #endregion
+
+        #region SOBRECARGAS IGUALDAD
+
+        public static bool operator !=(Dolar d, Euro e) { }
+        public static bool operator !=(Dolar d, Pesos p) { }
+        public static bool operator !=(Dolar d1, Dolar d2) { }
+
+        #endregion
+
+        #region SOBRECARGAS RESTA
+
+        public static Dolar operator -(Dolar d, Euro e) { }
+
+        #endregion
+
+    }
+}
