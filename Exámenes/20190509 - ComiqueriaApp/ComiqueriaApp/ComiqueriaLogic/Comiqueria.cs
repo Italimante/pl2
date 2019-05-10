f5a22f4e93c0587a063c9d6cc113dbfacb335177
streppelandres <andres.streppel@gmail.com>
2019-05-09 21:13:40 -0300

    pp

---
+++ Exámenes/20190509 - ComiqueriaApp/ComiqueriaApp/ComiqueriaLogic/Comiqueria.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComiqueriaLogic
+{
+    public class Comiqueria
+    {
+        #region Campos
+        private List<Producto> productos;
+        private List<Venta> ventas;
+        #endregion
+
+        #region Propiedades
+        /// <summary>
+        /// Tendrá un indexador que recibirá como parámetro un código Guid y devolverá el producto
+        /// de la lista de productos que corresponda con ese código.
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <returns></returns>
+        public Producto this[Guid codigo]
+        {
+            get
+            {
+                for(int i = 0; i < productos.Count; i++)
+                {
+                    if((Guid)productos[i] == codigo)
+                    {
+                        return productos[i];
+                    }
+                }
+
+                return null;
+            }
+        }
+        #endregion
+
+        #region Constructor
+        
+        /// <summary>
+        /// Instancio ambas listas
+        /// </summary>
+        public Comiqueria()
+        {
+            this.productos = new List<Producto>();
+            this.ventas = new List<Venta>();
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// ListarProductos devuelve un Dictionary<Guid, string>. Cada elemento del diccionario
+        ///corresponderá con cada producto en la lista de productos.La key será el código del
+        ///producto y el valor la descripción del producto.
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<Guid, string> ListarProductos()
+        {
+            Dictionary<Guid, string> dic = new Dictionary<Guid, string>();
+
+            for (int i = 0; i < productos.Count; i++)
+            {
+                dic.Add((Guid)productos[i], productos[i].Descripcion);
+            }
+
+            return dic;
+        }
+
+        /// <summary>
+        /// ListarVentas devuelve un string conteniendo la descripción breve de cada venta en la lista
+        ///de ventas.Una descripción por línea.Utilizar las herramientas que expone la clase Venta.
+        ///La lista deberá estar ordenada por fecha de la más reciente a la más antigua (Para
+        ///esto utilice el mecanismo que conozca y prefiera. Si lo necesita, agregue un método extra o
+        ///lo que requiera).
+        /// </summary>
+        /// <returns></returns>
+        public string ListarVentas()
+        {
+            StringBuilder sb = new StringBuilder();
+            
+            for (int i = 0; i < this.ventas.Count; i++)
+            {
+                sb.AppendFormat("\n{0}", this.ventas[i].ObtenerDescripcionBreve());
+            }
+
+            return "";
+        }
+
+        /// <summary>
+        /// El método Vender agrega una nueva venta a la lista de ventas.
+        /// </summary>
+        /// <param name="producto">Producto a agregar</param>
+        /// <param name="cantidad">Cantidades del producto</param>
+        public void Vender(Producto producto, int cantidad)
+        {
+            Venta test = new Venta(producto, cantidad); //mmmmmmmmm
+            this.ventas.Add(test);
+        }
+
+        public void Vender(Producto producto)
+        {
+            this.Vender(producto, 1);
+        }
+
+        #endregion
+
+        #region Sobrecargas
+
+        /// <summary>
+        /// La sobrecarga del operador == debe verificar si el producto se encuentra en la lista de
+        ///productos comparando la descripción.Si dos productos tienen la misma descripción son el
+        ///mismo producto.
+        /// </summary>
+        /// <param name="comiqueria"></param>
+        /// <param name="producto"></param>
+        /// <returns></returns>
+        public static bool operator ==(Comiqueria comiqueria, Producto producto)
+        {
+            for(int i = 0; i < comiqueria.productos.Count; i++)
+            {
+                if (comiqueria.productos[i].Descripcion == producto.Descripcion)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool operator !=(Comiqueria comiqueria, Producto producto)
+        {
+            return !(comiqueria == producto);
+        }
+
+        /// <summary>
+        /// La sobrecarga del operador + debe agregar un producto a la lista de producto siempre que
+        /// el mismo ya no exista en la lista.
+        /// </summary>
+        /// <param name="comiqueria"></param>
+        /// <param name="producto"></param>
+        /// <returns></returns>
+        public static Comiqueria operator +(Comiqueria comiqueria, Producto producto)
+        {
+            if(comiqueria == producto)
+            {
+                return comiqueria;
+            }
+
+            comiqueria.productos.Add(producto);
+
+            return comiqueria;
+        }
+
+        #endregion
+
+    }
+}
---
+++ Exámenes/20190509 - ComiqueriaApp/ComiqueriaApp/ComiqueriaLogic/Venta.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ComiqueriaLogic
+{
+    public sealed class Venta
+    {
+        #region Campos
+        private DateTime fecha;
+        private static int porcentajeIva;
+        private double precioFinal;
+        private Producto producto;
+        #endregion
+
+        #region Propiedades
+
+        /// <summary>
+        /// Tiene una propiedad que devuelve la fecha de la venta. Es de sólo lectura y tiene el mismo
+        /// modificador de acceso que el constructor de instancia.
+        /// </summary>
+        internal DateTime Fecha
+        {
+            get { return this.fecha; }
+        }
+        #endregion
+
+        #region Constructores
+
+        static Venta()
+        {
+            Venta.porcentajeIva = 21;
+        }
+
+        /// <summary>
+        /// El constructor de instancia inicializará el campo producto y llamará al método Vender
+        /// pasándole la cantidad indicada como argumento.
+        /// </summary>
+        /// <param name="producto"></param>
+        /// <param name="cantidad"></param>
+        internal Venta(Producto producto, int cantidad)
+        {
+            this.producto = producto;
+            this.Vender(cantidad);
+        }
+
+
+        #endregion
+
+        #region Métodos
+        /// <summary>
+        /// Le restará al stock del producto la cantidad que le pasaron por parámetro.
+        ///ii.Inicializará el campo fecha con la fecha actual completa.
+       ///iii.Inicializará el campo precioFinal con el valor retornado por el método
+      ///CalcularPrecioFinal, al cual se le pasará el precio unitario del producto y la cantidad.
+        /// </summary>
+        /// <param name="cantidad"></param>
+        private void Vender(int cantidad)
+        {
+            this.fecha = DateTime.Now;
+            this.precioFinal = CalcularPrecioFinal(this.producto.Precio, this.producto.Stock);
+        }
+
+
+        /// <summary>
+        /// El método ObtenerDescripcionBreve devuelve un string breve y en una sola línea indicando
+        /// fecha, descripción del producto y precio final.De esta forma: “fecha – descripción –
+        ///precioFinal”
+        /// </summary>
+        /// <returns></returns>
+        public string ObtenerDescripcionBreve()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("\nFecha {0} -", this.Fecha);
+            sb.AppendFormat("Descripción {0} -", this.producto.Descripcion);
+            sb.AppendFormat("Precio final {0}\n", this.precioFinal); //Esto en dos decimales
+            return sb.ToString();
+        }
+
+        /// <summary>
+        /// Calculará el precio final multiplicando
+        /// el precio unitario por la cantidad comprada y a este resultado le aplicará el porcentaje de
+        /// IVA que esté indicado en el campo porcentajeIva.
+        /// </summary>
+        /// <param name="precioUnidad"></param>
+        /// <param name="cantidad"></param>
+        /// <returns></returns>
+        public static double CalcularPrecioFinal(double precioUnidad, int cantidad)
+        {
+            double aux = precioUnidad * cantidad;
+            aux = (aux * 100) / Venta.porcentajeIva;
+            return aux + (precioUnidad * cantidad);
+        }
+
+        #endregion
+
+    }
+}
