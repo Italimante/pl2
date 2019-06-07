c40606d3cc35e65d77a7f47c1d28ef4204f9cf3b
streppelandres <italimante@gmail.com>
2019-06-06 21:56:02 -0300

    asd

---
+++ Guia/Ejercicio61/Formulario.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio61
+{
+    public partial class Formulario : Form
+    {
+        public Formulario()
+        {
+            InitializeComponent();
+            Persona p = new Persona("Julián", "Fernandez");
+            PersonaDAO.GuardarPersona(p);
+            Persona s = PersonaDAO.LeerPorID(2);
+
+            MessageBox.Show(s.ToString());
+            
+        }
+    }
+}
