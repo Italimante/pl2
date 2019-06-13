18b602c673906676c14451e19afef5df54d2fbb0
Andy <italimante@gmail.com>
2019-06-13 17:43:13 -0300

    63

--- Guia/Ejercicio63/Form1.Designer.cs
+++ Guia/Ejercicio63/Form1.Designer.cs
@@ -58,6 +58,7 @@
             this.Controls.Add(this.richTextBox1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Guia/Ejercicio63/Form1.cs
+++ Guia/Ejercicio63/Form1.cs
@@ -16,23 +16,28 @@ namespace Ejercicio63
 
     public partial class Form1 : Form
     {
+
+        Thread hilo;
+
         public Form1()
         {
             InitializeComponent();
 
+            this.labelHora.Text = DateTime.Now.ToString();
+
             ThreadStart delegado = new ThreadStart(AsignarHora);
-            Thread hilo = new Thread(delegado);
-            hilo.Start();
+            this.hilo = new Thread(delegado);
+            
+            this.hilo.Start();
         }
 
         public void AsignarHora()
         {
             DateTime t = DateTime.Now;
-            this.labelHora.Text = t.ToString();
 
             for (int i = 0; i < 100; i++)
             {
-                Thread.Sleep(1000);
+                Thread.Sleep(100);
                 this.CambiarHora(DateTime.Now);
             }
 
@@ -40,17 +45,26 @@ namespace Ejercicio63
 
         }
 
-        private void CambiarHora(DateTime t)
+        public void CambiarHora(DateTime t)
         {
             if (this.InvokeRequired)
             {
                 CambiarHoraDelegado delegado = new CambiarHoraDelegado(CambiarHora);
-                object[] parametros = { t };
+                object[] parametros = new object[] { t };
+                
                 this.Invoke(delegado, t);
             }
             else
             {
                 this.labelHora.Text = t.ToString();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (this.hilo.IsAlive)
+            {
+                this.hilo.Abort();
             }
         }
     }
