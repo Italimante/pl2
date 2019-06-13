59c0f0b9371b8bb1daf1fb14e2354f5fbb5f2ad4
streppelandres <andres.streppel@gmail.com>
2019-06-12 22:12:29 -0300

    A ksita

--- Guia/Ejercicio63/Form1.cs
+++ Guia/Ejercicio63/Form1.cs
@@ -7,21 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Ejercicio63
 {
+
+    delegate void CambiarHoraDelegado(DateTime t);
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            ThreadStart delegado = new ThreadStart(AsignarHora);
+            Thread hilo = new Thread(delegado);
+            hilo.Start();
         }
 
         public void AsignarHora()
         {
-            DateTime d = DateTime.Now;
+            DateTime t = DateTime.Now;
+            this.labelHora.Text = t.ToString();
 
-            this.labelHora.Text
+            for (int i = 0; i < 100; i++)
+            {
+                Thread.Sleep(1000);
+                this.CambiarHora(DateTime.Now);
+            }
+
+            MessageBox.Show("Terminó de medir la hora");
+
+        }
+
+        private void CambiarHora(DateTime t)
+        {
+            if (this.InvokeRequired)
+            {
+                CambiarHoraDelegado delegado = new CambiarHoraDelegado(CambiarHora);
+                object[] parametros = { t };
+                this.Invoke(delegado, t);
+            }
+            else
+            {
+                this.labelHora.Text = t.ToString();
+            }
         }
     }
 }
