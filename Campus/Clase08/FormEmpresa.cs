843f652697839ea579ece96b99d12ba383caba4c
Andy <italimante@gmail.com>
2019-07-23 16:14:46 -0300

    8 a medias

---
+++ Campus/Clase08/FormEmpresa.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Clase08
+{
+    public partial class FormEmpresa : Form
+    {
+
+        public Empresa empresa;
+
+        public FormEmpresa()
+        {
+            InitializeComponent();
+            this.Text = "Empresa";
+            this.AutoSizeMode = AutoSizeMode.GrowAndShrink;
+            this.StartPosition = FormStartPosition.CenterScreen;
+            this.MaximizeBox = false;
+        }
+
+        private void buttonAceptar_Click(object sender, EventArgs e)
+        {
+            this.empresa = new Empresa(
+                this.textBoxRazonSocial.Text, 
+                this.textBoxDireccion.Text, 
+                float.Parse(this.maskedTextBoxGanancias.Text)
+                );
+
+            this.DialogResult = DialogResult.OK;
+
+            this.Close();
+
+        }
+    }
+}
