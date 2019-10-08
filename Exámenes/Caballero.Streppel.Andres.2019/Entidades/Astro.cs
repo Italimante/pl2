0861408633f6eb1ceac4d82969b2f035b3a8e876
alumno <alumno@utnfra.com.ar>
2019-10-08 20:56:51 -0300

    push

---
+++ Exámenes/Caballero.Streppel.Andres.2019/Entidades/Astro.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public abstract class Astro
+    {
+        private int duracionOrbita;
+        private int duracionRotacion;
+        protected string nombre;
+
+        public Astro(int duraOrbita, int duraRotacion, string nombre) {
+            this.duracionOrbita = duraOrbita;
+            this.duracionRotacion = duraRotacion;
+            this.nombre = nombre;
+        }
+
+        protected string Mostrar() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("\nDuracion orbita: {0}", this.duracionOrbita);
+            sb.AppendFormat("\nDuracion rotacion: {0}", this.duracionRotacion);
+            sb.AppendFormat("\nNombre: {0}", this.nombre);
+            return sb.ToString();
+        }
+
+        public abstract string Orbitar();
+
+        public virtual string Rotar() {
+            return "Rotando. Tiempo Estimado: {0}" + this.duracionOrbita;
+        }
+
+        //TODO: Agregar un conversor explícito de Astro a String que retorne sólo el nombre del astro.
+
+
+
+
+
+    }
+}
---
+++ Exámenes/Caballero.Streppel.Andres.2019/Entidades/SistemaSolar.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    class SistemaSolar
+    {
+        private List<Astro> planetas;
+
+        public List<Astro> Planeta { get { return this.planetas; } }
+
+        /// <summary>
+        /// 22. MostrarInformacionAstros(): método que retorna toda la información de los planetas, y sus satélites.
+        /// </summary>
+        /// <returns></returns>
+        public String MostrarInformacionAstros() {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendFormat("\nLista de planetas del Sistema solar:");
+            foreach (Planeta p in this.planetas)
+            {
+                sb.AppendFormat("\n{0}", p.ToString());
+            }
+
+            return sb.ToString();
+        }
+
+        private SistemaSolar() {
+            this.planetas = new List<Astro>();
+        }
+
+    }
+}
